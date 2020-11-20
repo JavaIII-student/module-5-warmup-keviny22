5c3342023f795b821abc90226804159e1b522c45
Kevin S. Young <keviny22@users.noreply.github.com>
2020-11-19 21:46:06 -0800

    inital commit

--- src/SquaresIntoRectangles/SqInRectTest.java
+++ src/SquaresIntoRectangles/SqInRectTest.java
@@ -11,45 +11,45 @@ public class SqInRectTest {
 
 	@Test
 	public void test1() {
-		assertEquals(new ArrayList<Integer>(Arrays.asList(3, 2, 1, 1)), SquaresIntoRectangles.sqInRect(5, 3));
+		assertEquals(new ArrayList<Integer>(Arrays.asList(3, 2, 1, 1)), (new SquaresIntoRectangles()).sqInRect(5, 3));
 	}
 	@Test
 	public void test2() {
-		assertEquals(null, SquaresIntoRectangles.sqInRect(5, 5));
+		assertEquals(null, (new SquaresIntoRectangles()).sqInRect(5, 5));
 	}
 	@Test
 	public void test3() {
-		assertEquals(new ArrayList<Integer>(Arrays.asList(3, 2, 1, 1)), SquaresIntoRectangles.sqInRect(3, 5));
+		assertEquals(new ArrayList<Integer>(Arrays.asList(3, 2, 1, 1)), (new SquaresIntoRectangles()).sqInRect(3, 5));
 	}
 	@Test
 	public void test4() {
-		assertEquals(new ArrayList<Integer>(Arrays.asList(14, 6, 6, 2, 2, 2)), SquaresIntoRectangles.sqInRect(20, 14));
+		assertEquals(new ArrayList<Integer>(Arrays.asList(14, 6, 6, 2, 2, 2)), (new SquaresIntoRectangles()).sqInRect(20, 14));
 	}
 	@Test
 	public void test5() {
-		assertEquals(new ArrayList<Integer>(Arrays.asList(14, 6, 6, 2, 2, 2)), SquaresIntoRectangles.sqInRect(14, 20));
+		assertEquals(new ArrayList<Integer>(Arrays.asList(14, 6, 6, 2, 2, 2)), (new SquaresIntoRectangles()).sqInRect(14, 20));
 	}
 	@Test
 	public void test6() {
-		assertEquals(new ArrayList<Integer>(Arrays.asList(32, 32, 32, 32, 32, 32, 32, 16, 16)), SquaresIntoRectangles.sqInRect(240, 32));
+		assertEquals(new ArrayList<Integer>(Arrays.asList(32, 32, 32, 32, 32, 32, 32, 16, 16)), (new SquaresIntoRectangles()).sqInRect(240, 32));
 	}
 	@Test
 	public void test7() {
-		assertEquals(new ArrayList<Integer>(Arrays.asList(230, 230, 165, 65, 65, 35, 30, 5, 5, 5, 5, 5, 5)), SquaresIntoRectangles.sqInRect(625, 230));
+		assertEquals(new ArrayList<Integer>(Arrays.asList(230, 230, 165, 65, 65, 35, 30, 5, 5, 5, 5, 5, 5)), (new SquaresIntoRectangles()).sqInRect(625, 230));
 	}
 	@Test
 	public void test8() {
-		assertEquals(new ArrayList<Integer>(Arrays.asList(230, 230, 230, 41, 41, 41, 41, 41, 25, 16, 9, 7, 2, 2, 2, 1, 1)), SquaresIntoRectangles.sqInRect(731, 230));
+		assertEquals(new ArrayList<Integer>(Arrays.asList(230, 230, 230, 41, 41, 41, 41, 41, 25, 16, 9, 7, 2, 2, 2, 1, 1)), (new SquaresIntoRectangles()).sqInRect(731, 230));
 	}
 	@Test
 	public void test9() {
-		assertEquals(new ArrayList<Integer>(Arrays.asList(14, 14, 9, 5, 4, 1, 1, 1, 1)), SquaresIntoRectangles.sqInRect(37, 14));
+		assertEquals(new ArrayList<Integer>(Arrays.asList(14, 14, 9, 5, 4, 1, 1, 1, 1)), (new SquaresIntoRectangles()).sqInRect(37, 14));
 	}
 	@Test
 	public void test10() {
-		assertEquals(new ArrayList<Integer>(Arrays.asList(1, 1)), SquaresIntoRectangles.sqInRect(2, 1));
+		assertEquals(new ArrayList<Integer>(Arrays.asList(1, 1)), (new SquaresIntoRectangles()).sqInRect(2, 1));
 	}
-	
+
 	public static List<Integer> sqInRectTest(int lng, int wdth) {
 		if (lng == wdth) return null;
 		if (lng < wdth) {
@@ -83,11 +83,11 @@ public class SqInRectTest {
                 24,13,59,32,34,51,12,68,34,100));
 		for (int i = 0; i < 10; i++) {
 			int rn = rnd.nextInt(29);
-			int f1 = someLengths.get(rn); 
+			int f1 = someLengths.get(rn);
 			int f2 = someWidths.get(rn);
 			System.out.println("Random test " + i);
-			assertEquals(SqInRectTest.sqInRectTest(f1, f2), SquaresIntoRectangles.sqInRect(f1, f2));
+			assertEquals(SqInRectTest.sqInRectTest(f1, f2), (new SquaresIntoRectangles()).sqInRect(f1, f2));
 		}
-		
+
 	}
 }
--- src/SquaresIntoRectangles/SquaresIntoRectangles.java
+++ src/SquaresIntoRectangles/SquaresIntoRectangles.java
@@ -1,4 +1,5 @@
 package SquaresIntoRectangles;
+import java.util.ArrayList;
 import java.util.List;
 /**
  * <a href="https://docs.google.com/document/d/1rZ8aTKWlYvHZj7bamhiDcmLd7L6SNTNWVtvmXaW44_Q/edit?usp=sharing">problem</a>
@@ -6,9 +7,26 @@ import java.util.List;
  *
  */
 public class SquaresIntoRectangles {
-  
-  public static List<Integer> sqInRect(int length, int width) {
-	  //Write code here:
-	  return null; 
+
+    List<Integer> list;
+    public SquaresIntoRectangles() {
+        list = new ArrayList<Integer>();
+
+    }
+
+  public List<Integer> sqInRect(int length, int width) {
+        if (length == width) return null;
+        int longestSide = length > width ? length: width;
+        int shortestSide = length < width ? length: width;
+        int count = longestSide / shortestSide; //this is the quantity of squares that can be cut out
+        for (int i = 0; i < count ; i++) {
+            list.add(shortestSide);
+        }
+
+        int remainder = longestSide % shortestSide; //if the remainder is not zero, we can carve up the rectangle more
+
+        while(remainder != 0) return sqInRect(shortestSide, remainder); //recursive call
+
+        return list;
   }
 }
